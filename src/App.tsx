cd09a2e5ea1def8062ff40a58a480dd226524a8a
Bowie <larissa.soaress@yahoo.com.br>
2025-06-03 12:52:11 -0300

    Implementação Favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
+import Home from './pages/Home'
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home/>
       <Footer/>
     </div>
   )
